4fa568e3081bed5a0855b226320ad6da89db686e
alonso769 <131563220+alonso769@users.noreply.github.com>
2026-01-14 12:53:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 1. Esto asegura que tu página cargue bien en https://usuario.github.io/repo/
+  base: './', 
+  
+  // 2. Esto le dice a Vite: "No crees una carpeta 'dist', crea una carpeta 'docs'"
+  build: {
+    outDir: 'docs',
+  }
 })
